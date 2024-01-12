07f6fe68a83b6faf9b9f86d2eb9459c180247927
Steven Rossi <sprossi@landmarkfisheries.com>
2024-01-12 02:12:36 -0800

    Testing weights 10

--- poast-fish.py
+++ poast-fish.py
@@ -26,8 +26,8 @@ zdat = pd.read_csv('URLs.csv')
 iseq = list(range(0,len(zdat),1))
 j = np.random.randint(0,len(zdat),size=1)
 w = zdat[zdat.columns[3]]
-i = random.choices( iseq, weights=w, k=1)
-i = np.asarray(2)
+i = random.choices( iseq, weights=1/w, k=1)
+i = np.asarray(i)
 
 z = zdat.iloc[i-1]
 
